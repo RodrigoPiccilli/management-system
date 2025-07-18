35ef826df3e8dc6dc8635cb92f539294aacd5f89
Rodrigo Piccilli <rgpiccil@ncsu.edu>
2025-07-18 05:38:03 -0400

    Added "Add Job" modal, refined data table setup.

--- frontend/src/app/jobs/columns.tsx
+++ frontend/src/app/jobs/columns.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
 
 export type Job = {
     id: string
@@ -12,7 +16,27 @@ export type Job = {
 export const columns: ColumnDef<Job>[] = [
   {
     accessorKey: "id",
-    header: "Name",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Job Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+
+            // {/* <div className="flex flex-row items-center cursor-pointer"
+            // // variant="ghost"
+            // >
+            // <h3 onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>Job Name</h3>
+            // <ArrowUpDown className="ml-2 h-4 w-4" />
+            // </div> */}
+        )
+      },
+      cell: ({ row }) => (
+        <span className="cursor-pointer hover:text-red-500">{row.getValue("id")}</span>
+      ),
   },
   {
     accessorKey: "material",
--- frontend/src/app/jobs/page.tsx
+++ frontend/src/app/jobs/page.tsx
@@ -2,6 +2,29 @@ import { Button } from "@/components/ui/button";
 import { TabsMenu } from "@/components/ui/TabsMenu";
 import { columns, Job } from "./columns"
 import { DataTable } from "./data-table"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 
 async function getData(): Promise<Job[]> {
@@ -28,9 +51,6 @@ export default async function JobsPage() {
 
     const data = await getData();
 
-
-
-
     return ( 
          <div>
              <TabsMenu/>
@@ -38,10 +58,67 @@ export default async function JobsPage() {
              <h1 className="text-5xl text-center mt-20 mb-5">NVR Jobs</h1>
              
             <div className="flex justify-center gap-7 w-full bg-amber-500">
-                <Button className="p-6 text-xl cursor-pointer">Add</Button>
-                <Button className="p-6 text-xl cursor-pointer">Search</Button>
-                <Button className="p-6 text-xl cursor-pointer">Filter</Button>
-                <Button className="p-6 text-xl cursor-pointer">Sort</Button>
+                <Dialog>
+                    <form>
+                        <DialogTrigger asChild>
+                            <Button className="p-6 text-xl cursor-pointer">Add</Button>
+                        </DialogTrigger>
+
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Add NVR Job</DialogTitle>
+                                <DialogDescription></DialogDescription>
+                            </DialogHeader>
+
+                            {/* <div className="grid gap-4">
+                                <div className="grid gap-3">
+                                <Label htmlFor="name-1">Name</Label>
+                                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                                </div>
+                                <div className="grid gap-3">
+                                <Label htmlFor="username-1">Username</Label>
+                                <Input id="username-1" name="username" defaultValue="@peduarte" />
+                                </div>
+                            </div> */}
+
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableHead>Job Name</TableHead>
+                                        <TableCell><Input placeholder="Job Name"/></TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableHead>Area Code</TableHead>
+                                        <TableCell><Input placeholder="Area Code"/></TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableHead>Stone</TableHead>
+                                        <TableCell><Input placeholder="Stone Type"/></TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableHead>Model</TableHead>
+                                        <TableCell><Input placeholder="Model"/></TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableHead>Direction</TableHead>
+                                        <TableCell><Input placeholder="Direction"/></TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableHead>Sink</TableHead>
+                                        <TableCell><Input placeholder="Sink Type"/></TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+
+                        <DialogFooter>
+                            <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                            </DialogClose>
+                            <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                        </DialogContent>
+                    </form>
+                    </Dialog>
              </div>
 
              <div className="container mx-auto py-10">
