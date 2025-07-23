dcec73f29c3452fdb037caac10b7982e929863d4
Rodrigo Piccilli <rgpiccil@ncsu.edu>
2025-07-23 00:27:11 -0400

    Feat: Setup routing for Tabs.

--- frontend/src/components/ui/TabsMenu.tsx
+++ frontend/src/components/ui/TabsMenu.tsx
@@ -1,27 +1,33 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-// | Element       | Tailwind Class               | Example                            |
-// | ------------- | ---------------------------- | ---------------------------------- |
-// | Primary Color | `indigo-600`                 | `bg-indigo-600`, `text-indigo-600` |
-// | Secondary     | `gray-500` / `gray-700`      | `hover:bg-gray-100`                |
-// | Background    | `gray-50` / `gray-100`       | Body/table backgrounds             |
-// | Borders       | `gray-200`                   | Table lines, input borders         |
-// | Text          | `gray-800` / `gray-900`      | Main text                          |
-// | Headings      | `gray-900`                   | Page titles                        |
-// | Muted Text    | `gray-500`                   | Sub-labels, placeholder text       |
-// | Accent Hover  | `indigo-700` or `indigo-500` | Buttons, row hover                 |
+import { useRouter } from "next/navigation"
 
 
 export function TabsMenu() {
-  return (
-    <Tabs defaultValue="jobs" className="bg-slate-800 py-2 text-white">
-        <TabsList className="h-15 bg-slate-100 text-slate-700">
-        <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="jobs">NVR</TabsTrigger>
-        <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="calendar">Homeowners</TabsTrigger>
-        <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="template">Payables</TabsTrigger>
-        {/* <TabsTrigger className="text-lg px-10" value="schedule">Schedule</TabsTrigger>
-        <TabsTrigger className="text-lg px-10" value="payment">Payment</TabsTrigger> */}
-        </TabsList>
-    </Tabs>
-  );
+
+    const router = useRouter();
+
+    const tabRoutes = {
+        nvr: "/nvr",
+        homeowners: "/homeowners",
+        payables: "/payables",
+    };
+
+
+    return (
+        <Tabs
+            defaultValue="nvr"
+            className="bg-slate-800 py-2 text-white"
+            onValueChange={(value: string) => {
+                const route = tabRoutes[value as keyof typeof tabRoutes];
+                if (route) router.push(route);
+            }}
+        >
+            <TabsList className="h-15 bg-slate-100 text-slate-700" >
+                <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="nvr">NVR</TabsTrigger>
+                <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="homeowners">Homeowners</TabsTrigger>
+                <TabsTrigger className="text-lg px-10 data-[state=active]:bg-indigo-600 data-[state=active]:text-white data-[state=active]:font-bold min-w-7 hover:bg-slate-300" value="payables">Payables</TabsTrigger>
+            </TabsList>
+        </Tabs>
+    );
 }
