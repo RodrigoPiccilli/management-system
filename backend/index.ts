c789673441e33b727ff10ec1a8efeb62175b9ab0
Rodrigo Piccilli <rgpiccil@ncsu.edu>
2025-08-04 14:39:31 -0400

    [Fix] Enabled open CORS to allow requests from all

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,12 @@ import homeownerRoutes from './routes/homeowners';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: true,        
+    credentials: true    
+}));
+
 app.use(express.json());
 
 app.use('/api/nvr', nvrRoutes);
